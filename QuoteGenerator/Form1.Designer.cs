f1312b5cd46371f93fa92b62e7b9ae7e4015f959
Jeff Spurlock <jspurlock54@gmail.com>
2017-06-23 14:08:03 -0700

    Initial commit. This is the Version 1.0, completed quote app, with exportable text file. No duplicate search yet.

---
+++ QuoteGenerator/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace QuoteGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.constructionComboBox = new System.Windows.Forms.ComboBox();
+            this.constructionButton = new System.Windows.Forms.Button();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.gussetTextBox = new System.Windows.Forms.TextBox();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.gussetLabel = new System.Windows.Forms.Label();
+            this.lipLabel = new System.Windows.Forms.Label();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.lipTextBox = new System.Windows.Forms.TextBox();
+            this.lengthTextBox = new System.Windows.Forms.TextBox();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.milLabel = new System.Windows.Forms.Label();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.milTextBox = new System.Windows.Forms.TextBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.quoteButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.saveQuotesButton = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // constructionComboBox
+            // 
+            this.constructionComboBox.FormattingEnabled = true;
+            this.constructionComboBox.Items.AddRange(new object[] {
+            "Bottom Seal Bag",
+            "Side Gusset Bag",
+            "Zipper Bag",
+            "Lip & Tape Bag",
+            "Tubing Roll",
+            "Gusseted Tubing Roll"});
+            this.constructionComboBox.Location = new System.Drawing.Point(26, 13);
+            this.constructionComboBox.Name = "constructionComboBox";
+            this.constructionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.constructionComboBox.TabIndex = 0;
+            // 
+            // constructionButton
+            // 
+            this.constructionButton.Location = new System.Drawing.Point(168, 13);
+            this.constructionButton.Name = "constructionButton";
+            this.constructionButton.Size = new System.Drawing.Size(75, 23);
+            this.constructionButton.TabIndex = 1;
+            this.constructionButton.Text = "Go!";
+            this.constructionButton.UseVisualStyleBackColor = true;
+            this.constructionButton.Click += new System.EventHandler(this.constructionComboBox_Click);
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(26, 64);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.widthTextBox.TabIndex = 2;
+            this.widthTextBox.Visible = false;
+            // 
+            // gussetTextBox
+            // 
+            this.gussetTextBox.Location = new System.Drawing.Point(144, 64);
+            this.gussetTextBox.Name = "gussetTextBox";
+            this.gussetTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gussetTextBox.TabIndex = 3;
+            this.gussetTextBox.Visible = false;
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(26, 45);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(35, 13);
+            this.widthLabel.TabIndex = 4;
+            this.widthLabel.Text = "Width";
+            this.widthLabel.Visible = false;
+            // 
+            // gussetLabel
+            // 
+            this.gussetLabel.AutoSize = true;
+            this.gussetLabel.Location = new System.Drawing.Point(141, 45);
+            this.gussetLabel.Name = "gussetLabel";
+            this.gussetLabel.Size = new System.Drawing.Size(40, 13);
+            this.gussetLabel.TabIndex = 5;
+            this.gussetLabel.Text = "Gusset";
+            this.gussetLabel.Visible = false;
+            // 
+            // lipLabel
+            // 
+            this.lipLabel.AutoSize = true;
+            this.lipLabel.Location = new System.Drawing.Point(141, 94);
+            this.lipLabel.Name = "lipLabel";
+            this.lipLabel.Size = new System.Drawing.Size(21, 13);
+            this.lipLabel.TabIndex = 9;
+            this.lipLabel.Text = "Lip";
+            this.lipLabel.Visible = false;
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(26, 94);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(40, 13);
+            this.lengthLabel.TabIndex = 8;
+            this.lengthLabel.Text = "Length";
+            this.lengthLabel.Visible = false;
+            // 
+            // lipTextBox
+            // 
+            this.lipTextBox.Location = new System.Drawing.Point(144, 113);
+            this.lipTextBox.Name = "lipTextBox";
+            this.lipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lipTextBox.TabIndex = 7;
+            this.lipTextBox.Visible = false;
+            // 
+            // lengthTextBox
+            // 
+            this.lengthTextBox.Location = new System.Drawing.Point(26, 113);
+            this.lengthTextBox.Name = "lengthTextBox";
+            this.lengthTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lengthTextBox.TabIndex = 6;
+            this.lengthTextBox.Visible = false;
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(141, 143);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(46, 13);
+            this.quantityLabel.TabIndex = 13;
+            this.quantityLabel.Text = "Quantity";
+            this.quantityLabel.Visible = false;
+            // 
+            // milLabel
+            // 
+            this.milLabel.AutoSize = true;
+            this.milLabel.Location = new System.Drawing.Point(26, 143);
+            this.milLabel.Name = "milLabel";
+            this.milLabel.Size = new System.Drawing.Size(20, 13);
+            this.milLabel.TabIndex = 12;
+            this.milLabel.Text = "Mil";
+            this.milLabel.Visible = false;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(144, 162);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityTextBox.TabIndex = 11;
+            this.quantityTextBox.Visible = false;
+            // 
+            // milTextBox
+            // 
+            this.milTextBox.Location = new System.Drawing.Point(26, 162);
+            this.milTextBox.Name = "milTextBox";
+            this.milTextBox.Size = new System.Drawing.Size(100, 20);
+            this.milTextBox.TabIndex = 10;
+            this.milTextBox.Visible = false;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(23, 196);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(84, 13);
+            this.priceLabel.TabIndex = 15;
+            this.priceLabel.Text = "Price Per Pound";
+            this.priceLabel.UseWaitCursor = true;
+            this.priceLabel.Visible = false;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(26, 215);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 14;
+            this.priceTextBox.UseWaitCursor = true;
+            this.priceTextBox.Visible = false;
+            // 
+            // quoteButton
+            // 
+            this.quoteButton.Location = new System.Drawing.Point(141, 212);
+            this.quoteButton.Name = "quoteButton";
+            this.quoteButton.Size = new System.Drawing.Size(100, 23);
+            this.quoteButton.TabIndex = 16;
+            this.quoteButton.Text = "Quote!";
+            this.quoteButton.UseVisualStyleBackColor = true;
+            this.quoteButton.Visible = false;
+            this.quoteButton.Click += new System.EventHandler(this.quoteButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(274, 22);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(167, 213);
+            this.outputLabel.TabIndex = 17;
+            this.outputLabel.Text = "label8";
+            // 
+            // saveQuotesButton
+            // 
+            this.saveQuotesButton.Location = new System.Drawing.Point(277, 325);
+            this.saveQuotesButton.Name = "saveQuotesButton";
+            this.saveQuotesButton.Size = new System.Drawing.Size(164, 23);
+            this.saveQuotesButton.TabIndex = 18;
+            this.saveQuotesButton.Text = "Save Quotes to File";
+            this.saveQuotesButton.UseVisualStyleBackColor = true;
+            this.saveQuotesButton.Click += new System.EventHandler(this.saveQuotesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 360);
+            this.Controls.Add(this.saveQuotesButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.quoteButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.milLabel);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.milTextBox);
+            this.Controls.Add(this.lipLabel);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.lipTextBox);
+            this.Controls.Add(this.lengthTextBox);
+            this.Controls.Add(this.gussetLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.gussetTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.constructionButton);
+            this.Controls.Add(this.constructionComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox constructionComboBox;
+        private System.Windows.Forms.Button constructionButton;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox gussetTextBox;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label gussetLabel;
+        private System.Windows.Forms.Label lipLabel;
+        private System.Windows.Forms.Label lengthLabel;
+        private System.Windows.Forms.TextBox lipTextBox;
+        private System.Windows.Forms.TextBox lengthTextBox;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label milLabel;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.TextBox milTextBox;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button quoteButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button saveQuotesButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ QuoteGenerator/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace QuoteGenerator
+{
+    
+
+
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        string type = "";
+        string[,] results = new string[25, 10];
+        int quoteNum = 0;
+
+        //This entire method is a very verbose chunk of code, that just chooses what text boxes and labels to show based on the construction
+        private void constructionComboBox_Click(object sender, EventArgs e)
+        {
+            //Once the button is clicked, they are all set to visible
+            quoteButton.Visible = true;
+            widthTextBox.Visible = true;
+            widthLabel.Visible = true;
+            lengthTextBox.Visible = true;
+            lengthLabel.Visible = true;
+            milTextBox.Visible = true;
+            milLabel.Visible = true;
+            quantityTextBox.Visible = true;
+            quantityLabel.Visible = true;
+            priceTextBox.Visible = true;
+            priceLabel.Visible = true;
+
+            //switch statement turns off irrelevant boxes and labels for the bagConstruction
+            switch (constructionComboBox.SelectedItem.ToString())
+            {
+                case "Bottom Seal Bag":
+                    type = "Bag";                   
+                    gussetTextBox.Visible = false;                                                       
+                    gussetLabel.Visible = false;
+                    lipTextBox.Visible = false;
+                    lipLabel.Visible = false;                                     
+                    break;
+                case "Side Gusset Bag":
+                    type = "Bag";
+                    gussetTextBox.Visible = true;
+                    gussetLabel.Visible = true;
+                    lipTextBox.Visible = false;
+                    lipLabel.Visible = false;
+                    break;
+                case "Zipper Bag":
+                    type = "Bag";
+                    gussetTextBox.Visible = false;
+                    gussetLabel.Visible = false;
+                    lipTextBox.Visible = true;
+                    lipLabel.Visible = true;
+                    break;
+                case "Lip & Tape Bag":
+                    type = "Bag";
+                    gussetTextBox.Visible = false;
+                    gussetLabel.Visible = false;
+                    lipTextBox.Visible = true;
+                    lipLabel.Visible = true;
+                    break;
+                case "Tubing Roll":
+                    type = "Roll";
+                    gussetTextBox.Visible = false;
+                    gussetLabel.Visible = false;
+                    lipTextBox.Visible = false;
+                    lipLabel.Visible = false;
+                    break;
+                case "Gusseted Tubing Roll":
+                    type = "Roll";
+                    gussetTextBox.Visible = true;
+                    gussetLabel.Visible = true;
+                    lipTextBox.Visible = false;
+                    lipLabel.Visible = false;
+                    break;
+                default:
+                    outputLabel.Text = "Please enter a valid construction.";
+                    break;
+            }
+            //Resets all values back to empty or 0 each time the bagConstruction button is clicked. 
+            widthTextBox.Text = "";
+            lengthTextBox.Text = "";
+            gussetTextBox.Text = "0";
+            lipTextBox.Text = "0";
+            milTextBox.Text = "";
+            quantityTextBox.Text = "";
+            priceTextBox.Text = "";
+        }
+        private void quoteButton_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            //bagConstruction will end up in results array
+            string bagConstruction = constructionComboBox.SelectedItem.ToString();
+           
+            //totalWeight will be calculated with custom methods below, based on bagConstruction
+            double totalWeight = 0;
+            
+            //this data is where all the user inputed data will be stored, so that it can easily be passed through a method as a single argument
+            double[] data = new double[6];
+            //GetItemData collects user input and assigns the data to the data array. This array should be emptied every 
+            //time the button is clicked, and is only accessed from within the click button method so I've declared it 
+            //within the method instead of outside the method.
+            data = GetItemData();
+
+            //This is where the results will results will write to at the end of the quoteButton_Click() method
+            string outputText;
+
+            //This is the target price per pound for the quote.
+            double priceNum = Convert.ToDouble(priceTextBox.Text);
+
+            //This the total sale amount for the quote. 
+            double totalSaleNum;
+
+            //Convert data[] array to variable Strings for both outputText, and results[] arrya
+            string width, gusset, length, lip, mil, qty, price, totalSale;
+            width = data[0].ToString();
+            gusset = data[1].ToString();
+            length = data[2].ToString();
+            lip = data[3].ToString();
+            mil = (data[4] / 1000).ToString();
+            qty = data[5].ToString();
+            price = priceNum.ToString("C");
+        
+
+           //determine which method is appropraite for the bag construction
+            if (type == "Bag")
+            {
+                totalWeight = BagCalculator(data);
+            }
+            else if (type == "Roll")
+            {
+                totalWeight = RollCalculator(data);
+            }
+
+            //Calculate total sale price
+            totalSaleNum = priceNum * totalWeight;
+            //Get sale price as a string
+            totalSale = totalSaleNum.ToString("C");
+
+            //based on bagConstrucion, the outputText is different. 
+            switch (bagConstruction)
+            {
+                case "Bottom Seal Bag":
+                    outputText = String.Format("{0}\n{1}\"x{2}\"x{3}\n{4} Total for {5} units at {6} per pound", bagConstruction, width, length, mil, totalSale, qty, price);
+                    break;
+                case "Side Gusset Bag":
+                    outputText = String.Format("{0}\n{1}\"x{2}\"x{3}\"x{4}\n{5} Total for {6} units at {7} per pound", bagConstruction, width, gusset, length, mil, totalSale, qty, price);
+                    break;
+                case "Zipper Bag":
+                    outputText = String.Format("{0}\n{1}\"x{2}\"+{3}\"x{4}\n{5} Total for {6} units at {7} per pound", bagConstruction, width, length, lip, mil, totalSale, qty, price);
+                    break;
+                case "Lip & Tape Bag":
+                    outputText = String.Format("{0}\n{1}\"x{2}\"+{3}\"x{4}\n{5} Total for {6} units at {7} per pound", bagConstruction, width, length, lip, mil, totalSale, qty, price);
+                    break;
+                case "Tubing Roll":
+                    outputText = String.Format("{0}\n{1}\"x{2}'x{3}\n{4} Total for {5} units at {6} per pound", bagConstruction, width, length, mil, totalSale, qty, price);
+                    break;
+                case "Gusseted Tubing Roll":
+                    outputText = String.Format("{0}\n{1}\"x{2}\"x{3}'x{4}\n{5} Total for {6} units at {7} per pound", bagConstruction, width, gusset, length, mil, totalSale, qty, price);
+                    break;
+                default:
+                    outputText = "Try again";
+                    break;
+            }
+            outputLabel.Text = outputText;
+
+            //Declare and initialize all quote data to be passed through the 
+            //SendToResults() method, which uses a loop to write all this data into a multidimensional array;
+            string[] quoteData = new string[10] {width, gusset, length, lip, mil, qty, price, totalWeight.ToString(), totalSale, bagConstruction };
+            /*
+             quote data array is:
+             0: width
+             1: gusset
+             2: length
+             3: lip
+             4: mil
+             5: qty
+             6: price (price per pound)
+             7: totalWeight
+             8: total Sale   
+             9: bagConstruction                    
+             */
+             //Send array to multidimensional results[,] array.
+            SendToResults(quoteData);
+            ++quoteNum;
+        }
+
+        
+        private double[] GetItemData()
+        {
+            //This Method grabs all the user data and returns it as an array to be passed into either of the calculator methods
+           
+            /*
+            Legend for data array records:
+            0: Width
+            1: Gusset
+            2: Length
+            3: Lip
+            4: mil
+            5: Quantity
+            
+             */
+            //The calculator needs them all as doubles, so all of the text is converted to doubles here.
+            //NOTE: THIS DOES NOT GRAB THE PRICE TEXT. The price per pound does not need to go into the claculator
+            //methods so the price is grabbed separately within the Main() method.
+            double width = Convert.ToDouble(widthTextBox.Text);
+            double gusset = Convert.ToDouble(gussetTextBox.Text);
+            double length = Convert.ToDouble(lengthTextBox.Text);
+            double lip = Convert.ToDouble(lipTextBox.Text);
+            double mil = Convert.ToDouble(milTextBox.Text);
+            double qty = Convert.ToDouble(quantityTextBox.Text);
+            double[] itemData = new double[6] { width, gusset, length, lip, mil, qty };
+            return itemData;
+        }
+
+        
+        private double BagCalculator(double[] itemData)
+        {
+            //Calculator for if the item is a bag.
+            double width, gusset, length, lip, mil, qty;
+            width = itemData[0];
+            gusset = itemData[1];
+            length = itemData[2];
+            lip = itemData[3];
+            mil = itemData[4];
+            qty = itemData[5];
+            double totalWeight = ((width + gusset) * (length + lip) * mil) / 15000 * qty;
+            return totalWeight;
+        }
+        
+        private double RollCalculator(double[] itemData)
+        {
+            //Calculator for if the item is a roll
+            double width, gusset, length, lip, mil, qty;
+            width = itemData[0];
+            gusset = itemData[1];
+            length = itemData[2];
+            lip = itemData[3];
+            mil = itemData[4];
+            qty = itemData[5];
+            double totalWeight = ((width + gusset) * length * mil * 12) / 15000 * qty;
+            return totalWeight;
+        }
+
+        //Method that takes the quote results and sends them to the results[,] array
+        private void SendToResults(string[] quoteData)
+        {
+            /* for(int numQuotes = 0; numQuotes < 25; ++numQuotes)
+            {
+
+                for (int itemRecord = 0; itemRecord < quoteData.Length; ++itemRecord)
+                {
+                    results[numQuotes, itemRecord] = quoteData[itemRecord];
+                }
+            }*/
+            for(int itemRecord = 0; itemRecord < quoteData.Length; ++itemRecord)
+            {
+                results[quoteNum, itemRecord] = quoteData[itemRecord];
+            }
+
+        }
+
+        //Event Method to save the array to a txt file.
+        private void saveQuotesButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.ShowDialog();
+            string path = saveFileDialog.FileName;
+
+            FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs);
+
+            for(int quoteNum = 0; quoteNum < 25; ++quoteNum)
+            {
+                if (results[quoteNum, 0] != null) 
+                {
+                    writer.WriteLine("Bag Type: " + results[quoteNum, 9]);
+                    writer.WriteLine("Width: " + results[quoteNum, 0]);
+                    writer.WriteLine("Gusset: " + results[quoteNum, 1]);
+                    writer.WriteLine("Length: " + results[quoteNum, 2]);
+                    writer.WriteLine("Lip: " + results[quoteNum, 3]);
+                    writer.WriteLine("Mil: " + results[quoteNum, 4]);
+                    writer.WriteLine("Qty: " + results[quoteNum, 5]);
+                    writer.WriteLine("Price Per Pound: " + results[quoteNum, 6]);
+                    writer.WriteLine("Total Weight: " + results[quoteNum, 7]);
+                    writer.WriteLine("Total Sale: " + results[quoteNum, 8]);
+                    writer.WriteLine("\n");
+                }
+
+            }
+            writer.Close();
+            fs.Close();
+        }
+    }
+}
